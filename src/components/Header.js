d7185f50319e8f3e7036bb8915b9711b3308a410
Oliver <oliverklein33@gmail.com>
2020-09-08 13:38:27 -0300

    Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,39 @@
+import React from 'react'
 import styled from 'styled-components'
 
-export default styled.header`
-
+const HeaderWrapper = styled.header`
+  display: flex;
+  width: 100%;
+  background: #000;
 `
+
+const HeaderTitle = styled.span`
+  flex: 1;
+  color: #FFF;
+  padding: 10px;
+`
+
+const HeaderMenu = styled.span`
+  display: flex;
+  justify-content: flex-end;
+  flex: 1;
+  color: #FFF;
+  padding: 10px;
+  margin-left: 5px;
+  
+`
+
+const Header = () => {
+  return (
+    <HeaderWrapper>
+      <HeaderTitle>
+        KRRL Bank
+      </HeaderTitle>
+      <HeaderMenu>
+        Bienvenido/a Perez, Juan &#x25BC;
+      </HeaderMenu>
+    </HeaderWrapper>
+  )
+}
+
+export default Header
