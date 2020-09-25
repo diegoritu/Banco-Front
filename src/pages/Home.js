1610a3bf9b844091efc06001919748fabe065d32
axelroitman <38115641+axelroitman@users.noreply.github.com>
2020-09-24 23:12:17 -0300

    Otros cambios

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -8,11 +8,9 @@ import Text from '../components/Text'
 import { useHistory } from 'react-router-dom';
 import styled from 'styled-components'
 
-
 const PayButton = styled.button`
   
   margin-top: 10px;
-  margin-bottom: 10px;
   margin-left: 20%;
   margin-right: auto;
   background-color: #000;
@@ -20,6 +18,7 @@ const PayButton = styled.button`
   border-radius: 2px;
   font-size: 20px;
   color: #fff;
+  width: auto;
   line-height: 48px;
   :active, :hover
   {
@@ -28,17 +27,43 @@ const PayButton = styled.button`
 
 `
 
+const TransferButton = styled.button`
+  
+  margin-top: 10px;
+  margin-left: 20px;
+  margin-right: auto;
+  background-color: #000;
+  border: none;
+  border-radius: 2px;
+  font-size: 20px;
+  color: #fff;
+  width: auto;
+  line-height: 48px;
+  :active, :hover
+  {
+    background: #646464;
+  }
+
+`
+
+const ButtonsContainer = styled.div`
+  height: 70px;
+`
 
 const Home = () => {
   const history = useHistory()
   const navigateToServicePay = () => history.push('/servicePay')
+  const navigateToTransfer = () => history.push('/transfer')
 
   return (
     <GlobalContainer id='globalContainer'>
       <Header id='header' />
       <Content id='content' url="background.png" direction='column'>
         <Text> Acciones </Text>
-        <PayButton onClick={navigateToServicePay} type="button"> Pagar Servicios </PayButton>
+        <ButtonsContainer>
+        <PayButton onClick={navigateToServicePay} type="button"> Pagar servicios </PayButton> 
+        <TransferButton onClick={navigateToTransfer} type="button"> Realizar transferencia </TransferButton>
+        </ButtonsContainer>
         <Text> Mis Cuentas </Text>
         <AccountTable accountType={'SAVINGS'} />
         <AccountTable accountType={'CHECKING'} />
