082ed01f82533ad66f406e50f80e892e1e9e2727
diegoritu <49616722+diegoritu@users.noreply.github.com>
2020-10-25 20:28:50 -0300

    Updates

---
+++ precache-manifest.990a20eeb0968058110ca6770d5db49c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a539b09fcfcba9385753bce3b5857150",
+    "url": "/Banco-Front/index.html"
+  },
+  {
+    "revision": "cb73ab47ff28441f38bc",
+    "url": "/Banco-Front/static/css/main.a320b832.chunk.css"
+  },
+  {
+    "revision": "91ac3bd23ec40da1c62f",
+    "url": "/Banco-Front/static/js/2.33dc048e.chunk.js"
+  },
+  {
+    "revision": "fe4cdd745462666d252711c3f664a082",
+    "url": "/Banco-Front/static/js/2.33dc048e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cb73ab47ff28441f38bc",
+    "url": "/Banco-Front/static/js/main.72745e87.chunk.js"
+  },
+  {
+    "revision": "e16c3d69283dd6eb20ba",
+    "url": "/Banco-Front/static/js/runtime-main.ca026ad5.js"
+  }
+]);
